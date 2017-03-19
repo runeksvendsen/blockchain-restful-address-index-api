c24fecf74fbd62399d050c6ac5a49063c4fe8e1b
Rune K. Svendsen <runesvend@gmail.com>
2017-03-19 11:41:05 +0100

    initial commit

---
+++ src/Network/Bitcoin/AddrIndex/API.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE DataKinds, LambdaCase, TypeOperators, OverloadedStrings, GeneralizedNewtypeDeriving #-}
+
+module Network.Bitcoin.AddrIndex.API
+( -- *Server API
+  BlockchainApi
+  -- *Endpoints (individual)
+, AllOuts
+, UnspentOuts
+, TxOutProof
+, PublishTx
+, RawCmd
+  -- *Re-exports
+, module Network.Bitcoin.AddrIndex.Types
+)
+where
+
+import           Network.Bitcoin.AddrIndex.Types
+import           Servant.API
+import qualified Network.Haskoin.Transaction    as HT
+import qualified Data.Aeson                     as JSON
+import qualified Data.Text                      as T
+
+
+-- |The API exposed by this server.
+type BlockchainApi =
+       AllOuts
+  :<|> UnspentOuts
+  :<|> TxOutProof
+  :<|> PublishTx
+  :<|> RawCmd
+
+
+type AllOuts =
+    "outputs"     :> Capture "address" Addr :> "all"        :> Get  '[JSON] [AddressFundingInfo]
+
+type UnspentOuts =
+    "outputs"     :> Capture "address" Addr :> "unspent"    :> Get  '[JSON] [AddressFundingInfo]
+
+type TxOutProof =
+    "txOutProof"  :> Capture "txid" HT.TxHash               :> Get  '[JSON] FundingProof
+
+type PublishTx =
+    "publishTx"   :> ReqBody '[JSON] PushTxReq              :> Post '[JSON] PushTxResp
+
+type RawCmd =
+    "rawCmd"      :> Capture "method" String
+                  :> CaptureAll "args" T.Text               :> Get  '[JSON] JSON.Value
+
